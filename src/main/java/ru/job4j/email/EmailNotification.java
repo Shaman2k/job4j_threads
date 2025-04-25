67386fffa3aec944ded0b5d655953f5701a203d6
Shaman2k <103325541+Shaman2k@users.noreply.github.com>
2025-04-25 10:11:33 +0300

    2. ExecutorService рассылка почты. [#63097 #541136]

--- src/main/java/ru/job4j/email/EmailNotification.java
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -9,12 +9,7 @@ public class EmailNotification {
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.username(), user.email());
         String body = String.format("Add a new event to %s", user.username());
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                send(subject, body, user.email());
-            }
-        });
+        pool.submit(() -> send(subject, body, user.email()));
     }
 
     public void send(String subject, String body, String email) {
